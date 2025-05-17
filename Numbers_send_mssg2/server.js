34617727ce988651696533970df9a8777316144a
oumaima <omaimaelbdraouy@gmail.com>
2025-05-17 19:06:19 +0100

    Projet propre sans fichiers lourds

--- Numbers_send_mssg2/server.js
+++ Numbers_send_mssg2/server.js
@@ -183,5 +183,7 @@ app.post('/start-session', upload.single('file'), async (req, res) => {
   }
 });
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Serveur lancé sur le port ${PORT}`);
+});
 
